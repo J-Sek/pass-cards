b097c472dc12e7098c01379cca6f234b3d9f8126
J-Sek <J-Sek@users.noreply.github.com>
2024-12-19 03:44:48 +0100

    footer tweaks

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,10 +5,9 @@ v-app.bg-surface-container
       .d-flex.align-center
         v-avatar(image='/images/icon.svg' size='32')
         .ml-3 #[b Pass Cards]
-    template(#append)
-      .d-flex.align-center.ga-2.align-self-stretch.mr-6
-        v-btn.bg-surface-dim(rounded='lg' size='36' icon='mdi-theme-light-dark' @click='toggleTheme' v-tooltip:bottom='"Toggle theme"')
-  v-footer.justify-center.border-t(app) &copy; 2024 - {{ new Date().getFullYear() }} | J_SEK Solutions
+  v-footer.border-t.pr-2(app)
+    .d-flex.align-center &copy; 2024 #[v-divider.mx-3.my-1(vertical opacity='.7')] J_SEK Solutions
+    v-btn.ml-auto.bg-surface-dim.my-n3(rounded='lg' size='small' icon='mdi-theme-light-dark' @click='toggleTheme' v-tooltip:bottom='"Toggle theme"')
   slot
 </template>
 
