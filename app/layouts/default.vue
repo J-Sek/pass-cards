f54257ace2b00bd8c85918486f87dada0e1660e9
J-Sek <J-Sek@users.noreply.github.com>
2024-12-19 04:03:24 +0100

    Title color on dark toolbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@ v-app.bg-surface-container
     v-app-bar-title
       .d-flex.align-center
         v-avatar(image='/images/icon.svg' size='32')
-        .ml-3 #[b Pass Cards]
+        .ml-3 #[b(:style='{ color: isDark ? "#e1b847" : "" }') Pass Cards]
   v-footer.border-t.pr-2(app)
     .d-flex.align-center &copy; 2024 #[v-divider.mx-3.my-1(vertical opacity='.7')] J_SEK Solutions
     v-btn.ml-auto.bg-surface-dim.my-n3(rounded='lg' size='small' icon='mdi-theme-light-dark' @click='toggleTheme' v-tooltip:bottom='"Toggle theme"')
@@ -12,5 +12,5 @@ v-app.bg-surface-container
 </template>
 
 <script setup lang="ts">
-const { toggleTheme } = useAppTheme()
+const { isDark, toggleTheme } = useAppTheme()
 </script>
